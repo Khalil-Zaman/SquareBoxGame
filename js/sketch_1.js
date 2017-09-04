eaf4ae6513db05ad73f6721c479a2c29fcc13ad4
dizazta <khalilzaman@hotmail.com>
2017-09-05 00:10:52 +0100

    Added Background

--- js/sketch_1.js
+++ js/sketch_1.js
@@ -1,13 +1,29 @@
+var bottombar = 30;
+
 function setup() {
   //createCanvas(640, 480);
-  createCanvas(windowWidth - 10,windowHeight);
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+	background(0);
+	background_setup();
+	if (mouseIsPressed) {
+	fill(0);
+	} else {
+	fill(255);
+	}
+	ellipse(mouseX, mouseY, 80, 80);
+}
+
+function background_setup(){
+	fill(157);
+	rect(0, windowHeight-bottombar, windowWidth, bottombar);
+	fill(255);
+	rect(0, (windowHeight-bottombar)/3, windowWidth, (windowHeight-bottombar)/3);
+	fill(0);
+}
+
+function level_1(){
+	
 }
